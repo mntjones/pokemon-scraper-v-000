dfef019ae38923b768f142cd8deee94c939c9f4c
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-30 19:16:05 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db, :id, :hp
+
+  
+  def initialize (pokemon)
+
+    @id = pokemon[:id]
+    @name = pokemon[:name]
+    @type = pokemon[:type]
+    @db = pokemon[:db]
+    @hp = pokemon[:hp]
+    
+  end
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def self.find (id, db)
+    # The find method creates a new Pokemon after selecting their row from the database by their id number.
+    
+    poke_name = db.execute("SELECT name FROM pokemon WHERE id = ?", id).flatten[0]
+    poke_type = db.execute("SELECT type FROM pokemon WHERE id = ?", id).flatten[0]
+    #poke_hp = db.execute("SELECT hp FROM pokemon WHERE id = ?", id).flatten[0]
+    
+    poke = self.new(id: id, name: poke_name, type: poke_type, db: db)
+    poke   
+  end
+  
 end
